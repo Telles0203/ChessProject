0b970e6fada54a770e044e94a6724700012a9058
Alexandre M Telles <alexandre.mtelles@gmail.com>
2023-01-25 12:21:32 -0300

    Creating class ChessPosition

--- ChessConsole/ChessConsole/Program.cs
+++ ChessConsole/ChessConsole/Program.cs
@@ -7,26 +7,24 @@ namespace ChessConsole
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Board board = new Board(8, 8);
-                board.InsertPiece(new Tower(Color.Black, board), new Position(0, 0));
-                board.InsertPiece(new Tower(Color.Black, board), new Position(0, 7));
-                board.InsertPiece(new King(Color.Black, board), new Position(0, 3));
-                Screen.PrintBoard(board);
-            }
-            catch (ExceptionBoard exception)
-            {
-                Console.WriteLine(exception.Message);
-            }
 
-            
+            ChessPosition chessPosition = new ChessPosition('a', 1);
+            Console.WriteLine(chessPosition);
+            Console.WriteLine();
+            Console.WriteLine(chessPosition.ToPosition());
 
-
-
-
-
-            
+            //try
+            //{
+            //    Board board = new Board(8, 8);
+            //    board.InsertPiece(new Tower(Color.Black, board), new Position(0, 0));
+            //    board.InsertPiece(new Tower(Color.Black, board), new Position(0, 7));
+            //    board.InsertPiece(new King(Color.Black, board), new Position(0, 3));
+            //    Screen.PrintBoard(board);
+            //}
+            //catch (ExceptionBoard exception)
+            //{
+            //    Console.WriteLine(exception.Message);
+            //}
 
         }
     }
