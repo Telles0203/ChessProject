ca3d7968e94b6a1c714717d627ff4c28c4c0a451
Alexandre M Telles <alexandre.mtelles@gmail.com>
2023-01-25 10:28:17 -0300

    Creating the Position Class

---
+++ ChessConsole/ChessConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using Board;
+
+namespace ChessConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Position position;
+            position = new Position(3, 4);
+            Console.WriteLine(position);
+        }
+    }
+}
